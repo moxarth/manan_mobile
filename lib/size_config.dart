88de73d97a58e0490155b76413f8b383442e6819
moxarth <moxarthrathod12@gmail.com>
2020-10-13 20:12:25 +0530

    Size Config file added

---
+++ lib/size_config.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQuery;
+  static double width;
+  static double height;
+  static double defaultSize;
+  static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQuery = MediaQuery.of(context);
+    width = _mediaQuery.size.width;
+    height = _mediaQuery.size.height;
+    orientation = _mediaQuery.orientation;
+  }
+
+  double getProportionateScreenHeight(double inputHeight) {
+    double screenHeight = SizeConfig.height;
+    return (inputHeight / 812.0) * screenHeight;
+  }
+
+  double getProportionateScreenWidth(double inputWidth) {
+    double screenWidth = SizeConfig.width;
+    return (inputWidth / 812.0) * screenWidth;
+  }
+}
